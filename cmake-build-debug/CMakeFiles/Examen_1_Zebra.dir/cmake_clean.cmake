5d0b5e5500a42a785a1780ee917adfa5a236b6e9
Aragorn2209 <ramonbust2209@gmail.com>
2017-08-14 08:00:22 -0600

    Examen_1_Zebra
    Examen

---
+++ cmake-build-debug/CMakeFiles/Examen_1_Zebra.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen_1_Zebra.dir/main.cpp.obj"
+  "CMakeFiles/Examen_1_Zebra.dir/Evaluador.cpp.obj"
+  "Examen_1_Zebra.pdb"
+  "Examen_1_Zebra.exe"
+  "Examen_1_Zebra.exe.manifest"
+  "libExamen_1_Zebra.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Examen_1_Zebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
